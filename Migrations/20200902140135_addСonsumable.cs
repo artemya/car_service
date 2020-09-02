4fd1b09b956adae1d25f358591099b13d35f7e6b
artemya <artemya@tahometer.com>
2020-09-02 17:03:35 +0300

    [Migrations] add migration addСonsumable

---
+++ Migrations/20200902140135_addСonsumable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace car_service.Migrations
+{
+    public partial class addСonsumable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Сonsumable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Сonsumable", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Сonsumable");
+        }
+    }
+}
