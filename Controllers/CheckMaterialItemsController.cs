1fd7910942044200c6ec3b8970fd3bff470c7980
artemya <artemya@tahometer.com>
2020-09-03 15:14:12 +0300

    [Controllers] add CheckMaterialItems controller

---
+++ Controllers/CheckMaterialItemsController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using car_service.API.Models;
+using car_service.API.Services;
+
+namespace car_service.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckMaterialItemsController : ControllerBase
+    {
+        private CheckMaterialItemService _checkMaterialItemService;
+        public CheckMaterialItemsController (CheckMaterialItemService checkMaterialItemService)
+        {
+            _checkMaterialItemService = checkMaterialItemService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<CheckMaterialItem>> Get()
+        {
+            
+            return _checkMaterialItemService.GetAllCheckItem();
+        }
+
+    }
+}
