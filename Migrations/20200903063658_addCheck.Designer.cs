5288837742a1820f6259f69ce42aa70e8741cc04
artemya <artemya@tahometer.com>
2020-09-03 09:39:10 +0300

    [Migrations] add Check migrations

--- Migrations/20200903063658_addCheck.Designer.cs
+++ Migrations/20200903063658_addCheck.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using car_service.API.Models;
 
 namespace car_service.Migrations
 {
     [DbContext(typeof(CarServiceDbContext))]
-    partial class CarServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200903063658_addCheck")]
+    partial class addCheck
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
