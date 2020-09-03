be07bf8410c8a500760e3813bd1d16058ec15706
artemya <artemya@tahometer.com>
2020-09-03 09:30:43 +0300

    [Controllers] add Client Controller

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace car_service
             services.AddScoped<CategoryService>();
             services.AddScoped<ServiceService>();
             services.AddScoped<ExpendableMaterialService>();
+            services.AddScoped<ClientService>();
             services.AddDbContext<CarServiceDbContext>(opt => opt.UseSqlite("Data Source=CarService.db"));
             services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
