15add003f068a95569065da8dfdb3a2c80792f64
artemya <artemya@tahometer.com>
2020-09-03 09:26:26 +0300

    [Services] add Client Service

---
+++ Services/ClientService.cs
@@ -0,0 +1,28 @@
+using car_service.API.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace car_service.API.Services
+{
+    public class ClientService
+    {
+        private readonly CarServiceDbContext _context;
+        public ClientService(CarServiceDbContext context) 
+        {
+            _context = context;
+        }
+
+        public ActionResult<List<Client>> GetAllClient()
+        {
+            return _context.Client.ToList();
+        }
+
+        public async Task<ActionResult<Client>> GetById(int id)
+        {
+            return await _context.Client.FindAsync(id);
+        }
+    }
+}
